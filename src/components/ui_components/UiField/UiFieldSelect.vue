3779f0ef8fb7016e524bf82d20c6acd50d535fdb
Viktor Seniukov <spinnekopser@gmail.com>
2023-11-21 21:21:10 +0400

    Create code first

---
+++ src/components/ui_components/UiField/UiFieldSelect.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+    interface IOptions {
+        id: number
+        value: string | number
+        caption: string
+    }
+
+    withDefaults(defineProps<{
+        modelValue: string,
+        name: string,
+        options: IOptions[],
+    }>(), {
+        options: () => [] as IOptions[],
+    });
+    defineEmits(['update:modelValue']);
+</script>
+
+<template>
+    <select
+        @change="$emit('update:modelValue', $event.target.value)"
+        :value="modelValue"
+        :name="name"
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+    >
+        <option
+            v-for="option in options"
+            :key="option.id"
+            :value="option.value"
+        >
+            {{ option.caption }}
+        </option>
+    </select>
+</template>
+
+<style scoped lang="scss">
+
+</style>
